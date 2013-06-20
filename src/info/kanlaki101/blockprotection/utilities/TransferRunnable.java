087f46fa3e62934c5c5e3b14494cb55cffb9d531
Peter Maatman <blackwolf12333@gmail.com>
2013-06-20 10:33:03 +0200

    Add /bpclean command to clean a worldedit selection of any protections

--- src/info/kanlaki101/blockprotection/utilities/TransferRunnable.java
+++ src/info/kanlaki101/blockprotection/utilities/TransferRunnable.java
@@ -26,23 +26,29 @@ public class TransferRunnable implements Runnable {
         final Selection select = pl.worldEdit.getSelection(p);
         final World world = p.getWorld();
 
-        p.sendMessage(ChatColor.RED + " [BProtect] Starting Transfer of Ownership. This may take a bit.");
+		if(transferTo != null) {
+			p.sendMessage(ChatColor.RED + " [BProtect] Starting Transfer of Ownership. This may take a bit.");
+		} else {
+			p.sendMessage(ChatColor.RED + " [BProtect] Starting cleanup of selected area. This may take a bit.");
+		}
         Location max = select.getMaximumPoint();
         Location min = select.getMinimumPoint();
         for (int x = min.getBlockX(); x <= max.getBlockX(); x++) {
             for (int y = min.getBlockY(); y <= max.getBlockY(); y++) {
                 for (int z = min.getBlockZ(); z <= max.getBlockZ(); z++) {
                     Location loc = new Location(world, x, y, z);
-                    if ((loc.getBlock().getType() != Material.AIR) && (loc.getBlock().getType() != Material.LAVA) &&
-                            (loc.getBlock().getType() != Material.WATER)) {
+                    if ((loc.getBlock().getType() != Material.AIR) && (loc.getBlock().getType() != Material.LAVA) && (loc.getBlock().getType() != Material.WATER)) {
                         BPBlockLocation blockLoc = new BPBlockLocation(x, y, z, world.getName());
                         if (pl.worldDatabases.get(blockLoc.getWorld()).containsKey(blockLoc)) {
                             String blockowner = pl.worldDatabases.get(blockLoc.getWorld()).get(blockLoc);
                             if (blockowner != null)
                                 pl.worldDatabases.get(blockLoc.getWorld()).remove(blockLoc);
                         }
-                        if (transferTo!=null)
-                        pl.worldDatabases.get(blockLoc.getWorld()).put(blockLoc, transferTo);
+                        if (transferTo!=null) {
+                        	pl.worldDatabases.get(blockLoc.getWorld()).put(blockLoc, transferTo);
+                        } else {
+                        	continue;
+                        }
                     }
                 }
             }
